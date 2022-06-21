6c0560213d5affaf59ee6e8f5a5815d05b8014eb
YunHe3 <924718724@qq.com>
2022-06-21 09:25:46 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,7 +2,15 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
-
+    public static boolean isLeapYear(int year){
+        if(year%400==0){
+            return true;
+        }
+        else if(year%4==0&&year%100!=0){
+            return true;
+        }
+        return false;
+    }
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
